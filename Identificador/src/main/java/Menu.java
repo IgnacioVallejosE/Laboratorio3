d21719fc89b91d05bd10f84bf70132611205ae4e
Nacho <i.vallejos01@ufromail.cl>
2021-06-09 09:35:21 -0400

    Segundo commit

--- Identificador/src/main/java/Menu.java
+++ Identificador/src/main/java/Menu.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class Menu {
+    public void menu(){
+        Scanner tec = new Scanner(System.in);
+        System.out.println("Elija a gusto\n" +
+                "");
+    }
 }
--- Identificador/src/main/java/RAM.java
+++ Identificador/src/main/java/RAM.java
@@ -1,2 +1,35 @@
 public class RAM {
+    private String marca;
+    private String capacidad;
+    private String frecuencia;
+
+    public RAM(String marca, String capacidad, String frecuencia) {
+        this.marca = marca;
+        this.capacidad = capacidad;
+        this.frecuencia = frecuencia;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getFrecuencia() {
+        return frecuencia;
+    }
+
+    public void setFrecuencia(String frecuencia) {
+        this.frecuencia = frecuencia;
+    }
 }
